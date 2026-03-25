27b83361a6b1391ea0f9ee9c64ade7099fd156f4
xNeto7 <mateja2006@icloud.com>
2026-03-25 09:25:54 +0100

    kurac

--- test.py
+++ test.py
@@ -3,68 +3,65 @@ import random
 
 WIDTH = 800
 HEIGHT = 600
-PLAYER_SPEED = 4
-
-class Player(arcade.Sprite):
-    def update(self):
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-class Enemy(arcade.Sprite):
-    def update(self):
-        self.center_x += random.choice([-2, -1, 0, 1, 2])
-        self.center_y += random.choice([-2, -1, 0, 1, 2])
+PLAYER_SPEED = 200
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(WIDTH, HEIGHT, "Mini RPG")
-        arcade.set_background_color(arcade.color.DARK_GREEN)
+        super().__init__(WIDTH, HEIGHT, "Smooth Movement with Keys")
+        arcade.set_background_color(arcade.color.BLACK)
 
-        self.player = Player(":resources:images/animated_characters/male_person/malePerson_idle.png", 1)
-        self.player.center_x = 400
-        self.player.center_y = 300
+        self.player = arcade.Sprite(":resources:images/animated_characters/male_person/malePerson_idle.png", 1)
+        self.player.center_x = WIDTH // 2
+        self.player.center_y = HEIGHT // 2
+
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player)
 
         self.enemies = arcade.SpriteList()
         for _ in range(5):
-            enemy = Enemy(":resources:images/enemies/slimeBlue.png", 1)
+            enemy = arcade.Sprite(":resources:images/enemies/slimeBlue.png", 1)
             enemy.center_x = random.randint(0, WIDTH)
             enemy.center_y = random.randint(0, HEIGHT)
             self.enemies.append(enemy)
 
-        self.score = 0
+        # Tasten-Tracking
+        self.keys_held = set()
 
     def on_draw(self):
         self.clear()
-        self.player.draw()
+        self.player_list.draw()
         self.enemies.draw()
-        arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 14)
 
     def on_update(self, delta_time):
-        self.player.update()
-        self.enemies.update()
+        # Geschwindigkeit zurücksetzen
+        self.player.change_x = 0
+        self.player.change_y = 0
 
-        # Kollision check
-        hit_list = arcade.check_for_collision_with_list(self.player, self.enemies)
-        for enemy in hit_list:
-            enemy.remove_from_sprite_lists()
-            self.score += 1
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.W:
+        # Tasten prüfen
+        if arcade.key.W in self.keys_held or arcade.key.UP in self.keys_held:
             self.player.change_y = PLAYER_SPEED
-        elif key == arcade.key.S:
+        if arcade.key.S in self.keys_held or arcade.key.DOWN in self.keys_held:
             self.player.change_y = -PLAYER_SPEED
-        elif key == arcade.key.A:
+        if arcade.key.A in self.keys_held or arcade.key.LEFT in self.keys_held:
             self.player.change_x = -PLAYER_SPEED
-        elif key == arcade.key.D:
+        if arcade.key.D in self.keys_held or arcade.key.RIGHT in self.keys_held:
             self.player.change_x = PLAYER_SPEED
 
-    def on_key_release(self, key, modifiers):
-        if key in [arcade.key.W, arcade.key.S]:
-            self.player.change_y = 0
-        if key in [arcade.key.A, arcade.key.D]:
-            self.player.change_x = 0
+        # Spieler bewegen
+        self.player.center_x += self.player.change_x * delta_time
+        self.player.center_y += self.player.change_y * delta_time
 
+        # Gegner bewegen
+        for enemy in self.enemies:
+            enemy.center_x += random.randint(-2, 2)
+            enemy.center_y += random.randint(-2, 2)
+
+    def on_key_press(self, key, modifiers):
+        self.keys_held.add(key)
+
+    def on_key_release(self, key, modifiers):
+        if key in self.keys_held:
+            self.keys_held.remove(key)
 
 if __name__ == "__main__":
     game = Game()
